b8a475fb0b47a604278ed0026fbbc6a5990f6f2f
Your Username <your-email@example.com>
2025-07-20 14:20:11 +0700

    google veriv

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,16 +9,19 @@ import "swiper/css"
 
 const Layout = ({ children }) => {
     useEffect(() => {
-        AOS.init({ duration: 1000, once: true }) // once=true biar animasi hanya sekali
+        AOS.init({ duration: 1000, once: true })
     }, [])
 
     return (
         <>
             <Head>
                 <title>Layanan Nusantara</title>
-                <meta name='description' content='' />
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
+                <meta name="description" content="" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+                
+                {/* ✅ Tambahkan meta tag verifikasi di sini */}
+                <meta name="google-site-verification" content="m75bMloU2miNejb-JdXZcuRjL2mv5nQDc4q_ZKJ0xsE" />
             </Head>
 
             <Navbar />
