c3d6701a550e1972066a1c6a9b93a11566a79732
Your Username <your-email@example.com>
2025-07-29 19:58:44 +0700

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://layanannusantara.netlify.app', // Ganti dengan URL websitemu
+  generateRobotsTxt: true, // otomatis bikin robots.txt
+  sitemapSize: 5000, 
+};
