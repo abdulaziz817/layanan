cc44921569e36060ee3703d60f78c2fa97c179c2
Your Username <your-email@example.com>
2025-12-11 14:41:10 +0700

    update favicon lagi

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -25,7 +25,11 @@ const Layout = ({ children }) => {
                 />
 
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-           <link rel="icon" href="/favicon/favicon-32x32.png" />
+           <link rel="icon" href="/favicon/favicon.ico" />
+
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+
 
 
 
