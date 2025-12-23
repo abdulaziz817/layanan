0c543b98d159eec73573993f3f54fe3549f0fba9
Your Username <your-email@example.com>
2025-12-23 16:11:43 +0700

    pavicon.ico

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -34,9 +34,9 @@ const Layout = ({ children }) => {
                 />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                <link rel="icon" href="/favicon/favicon.ico" />
-                <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
-                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+                <link rel="icon" href="/favicon.ico" />
+                {/* <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" /> */}
 
                 <meta property="og:site_name" content="Layanan Nusantara" />
                 <meta property="og:title" content="Layanan Nusantara" />
