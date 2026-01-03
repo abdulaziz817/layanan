53922ba4b9c86b36da4eebd29113c5d70067f200
Your Username <your-email@example.com>
2026-01-03 11:52:19 +0700

    pembuatan app

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className='scroll-smooth' lang='en'>
-      <Head />
-      <body className='md:overflow-visible'>
+    <Html className="scroll-smooth" lang="id">
+      <Head>
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+
+      <body className="md:overflow-visible">
         <Main />
         <NextScript />
       </body>
